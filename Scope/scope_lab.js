37be0a1d07839260a15ee6976704a20f455df3d5
yashasvidasi <yashasvidasi@gmail.com>
2024-01-21 00:48:53 -0500

    msg

---
+++ Scope/scope_lab.js
@@ -0,0 +1,12 @@
+var globalvar = "hehe";
+let globallet = "raharha";
+const globalc = 123;
+
+{
+    var blockv = "hahah";
+    let blockl = "rere";
+    const blockc = 321;
+    console.log(blockl);
+}
+
+console.log(globalvar);
